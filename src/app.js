c2395382dd4b25c827bff0cc702cadfbfdf355aa
dev-mabdulrehman <dev.mabdulrehman@gmail.com>
2021-05-29 16:35:57 +0500

    Navlinks

--- src/app.js
+++ src/app.js
@@ -1,156 +1,123 @@
 require('dotenv').config();
-const express = require("express");
-//const { Console } = require("node:console");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
-const hbs = require("hbs");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const cookieParser = require("cookie-parser");
-const auth = require("./middleware/auth");
+const hbs = require('hbs');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const auth = require('./middleware/auth');
 
-require("./db/conn");
-const Register = require("./models/registers");
-const { json } = require("express");
-const { log } = require("console");
+require('./db/conn');
+const Register = require('./models/registers');
+const { json } = require('express');
+const { log } = require('console');
 
 const port = process.env.PORT || 4000;
- 
-const static_path = path.join(__dirname, "../public");
-const template_path = path.join(__dirname, "../templates/views");
-const partials_path = path.join(__dirname, "../templates/partials");
 
+const static_path = path.join(__dirname, '../public');
+const template_path = path.join(__dirname, '../templates/views');
+const partials_path = path.join(__dirname, '../templates/partials');
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(static_path));
-app.set("view engine", "hbs");
-app.set("views", template_path);
+app.set('view engine', 'hbs');
+app.set('views', template_path);
 hbs.registerPartials(partials_path);
 
-//console.log(process.env.SECRET_KEY)
-app.get("/", (req, res) => {
-    res.render("index");
+app.get('/', (req, res) => {
+	let jwToken = req.cookies.jwt;
+	let isLogin = false;
+	if (jwToken != null) {
+		isLogin = true;
+	}
+	console.log(`this is our cookie${req.cookies.jwt}`);
+	res.render('index', { isLogin });
 });
 
-app.get("/webpage", auth , (req, res) => {
-    //console.log(`this is our cookie${req.cookies.jwt}`);
-    res.render("webpage")
-});
- 
-app.get("/logout" , auth,  async(req, res)=>{
-    try {
-        req.user.tokens = req.user.tokens.filter((currElement)=>{
-            return currElement.token !== req.token;
-        })
-        res.clearCookie("jwt");
-        console.log("logout successfully")
-        await req.user.save();
-        res.render("login");
-    } catch (error) {
-        res.status(500).send(error);
-        
-    }
-})
-
-
-app.get("/register", (req, res) => {
-    res.render("register");
-});
-app.get("/login", (req, res) => {
-    res.render("login");
-});
-app.post("/register", async (req, res) =>{
-    try {
-
-      const password = req.body.password;
-      const cpassword = req.body.confirmpassword;
-
-      if(password === cpassword){
-        
-        const registerEmployee = new Register({
-                firstname: req.body.firstname,
-                lastname:req.body.lastname,
-                email:req.body.email,
-                gender:req.body.gender,
-                phone:req.body.phone,
-                age:req.body.age,
-                password:req.body.password,
-                confirmpassword:req.body.confirmpassword    
-        })
-
-         console.log("the success part" + registerEmployee);
-
-        const token = await registerEmployee.generateAuthToken();
-        console.log("the token part" + token);
-
-        res.cookie("jwt", token, {
-            expires:new Date(Date.now() + 600000),
-        });
-
-        const registered = await registerEmployee.save();
-         console.log("the page part" + registered);
-
-        res.status(201).render("index");
-
-      }else{
-          res.send("password are not matching")
-      }
-        
-    } catch (error) {
-        res.status(400).send(error);
-        console.log("the error part page ");
-    }
-})
-app.post("/login", async(req, res) => {
-    try {
-        const email = req.body.email;
-        const password = req.body.password;
-
-        //console.log(`${email} and password is ${password}`)
-        const useremail = await Register.findOne({email:email});
-
-        const isMatch = await bcrypt.compare(password, useremail.password);
-
-        const token = await useremail.generateAuthToken();
-        console.log("the token part" + token);
-        res.cookie("jwt", token, {
-            expires:new Date(Date.now() + 600000),
-        });
-
-
-       
-        if(isMatch){
-            res.status(201).render("index");
-        }else{
-            res.send(error.message);
-        }
-
-    } catch (error) {
-        res.status(400).send(error.message);
-    }
+app.get('/webpage', auth, (req, res) => {
+	//console.log(`this is our cookie${req.cookies.jwt}`);
+	res.render('webpage');
 });
 
+app.get('/logout', auth, async (req, res) => {
+	try {
+		req.user.tokens = req.user.tokens.filter(currElement => {
+			return currElement.token !== req.token;
+		});
+		res.clearCookie('jwt');
+		await req.user.save();
+		res.render('login');
+	} catch (error) {
+		res.status(500).send(error);
+	}
+});
 
+app.get('/register', (req, res) => {
+	res.render('register');
+});
+app.get('/login', (req, res) => {
+	res.render('login');
+});
+app.post('/register', async (req, res) => {
+	try {
+		const password = req.body.password;
+		const cpassword = req.body.confirmpassword;
 
+		if (password === cpassword) {
+			const registerEmployee = new Register({
+				firstname: req.body.firstname,
+				lastname: req.body.lastname,
+				email: req.body.email,
+				gender: req.body.gender,
+				phone: req.body.phone,
+				age: req.body.age,
+				password: req.body.password,
+				confirmpassword: req.body.confirmpassword,
+			});
 
+			const token = await registerEmployee.generateAuthToken();
 
-// const bcrypt = require("bcryptjs");
+			res.cookie('jwt', token, {
+				expires: new Date(Date.now() + 600000),
+			});
 
-// const securePassword = async (password) =>{
+			const registered = await registerEmployee.save();
 
-//     const passwordHash = await bcrypt.hash(password, 10);
-//     console.log(passwordHash);
+			res.status(201).render('index');
+		} else {
+			res.send('password are not matching');
+		}
+	} catch (error) {
+		res.status(400).send(error);
+	}
+});
+app.post('/login', async (req, res) => {
+	try {
+		const email = req.body.email;
+		const password = req.body.password;
+		const useremail = await Register.findOne({ email: email });
 
-//     const passwordmatch = await bcrypt.compare("thapa@13", passwordHash);
-//     console.log(passwordmatch);
-    
-    
-// }    
-//     securePassword("thapa@123");
+		const isMatch = await bcrypt.compare(password, useremail.password);
 
-app.listen(port, ()=> {
-    console.log(`server iss running on port ${port}`);
+		const token = await useremail.generateAuthToken();
+		res.cookie('jwt', token, {
+			expires: new Date(Date.now() + 600000),
+		});
+
+		if (isMatch) {
+			res.status(201).render('index');
+		} else {
+			res.send(error.message);
+		}
+	} catch (error) {
+		res.status(400).send(error.message);
+	}
+});
+
+app.listen(port, () => {
+	console.log(`server iss running on port ${port}`);
 });
